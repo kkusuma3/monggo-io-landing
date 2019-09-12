b6d2989c5f0cb639218cc4747f6b9ab4ed8afa9d
yosiassapari <yosiassapari@gmail.com>
2019-09-12 03:36:35 -0700

    making the basic index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,26 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Opening your room to<br/>a world of possibilities</h1>
+    <p>An app made for hotels<br/>So travelers can worry less and travel more.</p>
+    <button>
+      <Link to="/sign-up/">Sign Up</Link>
+    </button>
+    <button>
+      <Link to="/learn-more/">Learn More</Link>
+    </button>
+    
+    {/* do not need this
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
