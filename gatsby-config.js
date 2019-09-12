eedcce76537404632249a883d5b06279a335e2da
yosiassapari <yosiassapari@gmail.com>
2019-09-12 03:15:53 -0700

    change the desc in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Monggo.io`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `An app made for hotels, so travelers can worry less and travel more.`,
     author: `@gatsbyjs`,
   },
   plugins: [
