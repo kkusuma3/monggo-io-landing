5e3d3856d9b6d8793a1694beb5cd647cf1feccbe
Kevin Kusuma <kevin.kusuma@revelsystems.com>
2019-09-17 20:53:00 -0400

    Updated header links, added styling and finished homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,43 +2,78 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const homeBackground = '/images/background.jpg';
 
 const useStyles = makeStyles((theme) => ({
-  imgBackground: {
-    background: `url(${homeBackground}) no-repeat center center fixed`,
-    backgroundSize: 'cover',
-    height: 'calc(100vh - 50px)',
-    width: '100%',
-  },
+    gridWrapper: {
+        padding: '7rem 0 0 3.5rem'
+    },
+    imgBackground: {
+        background: `url(${homeBackground}) no-repeat center center fixed`,
+        backgroundSize: 'cover',
+        height: 'calc(100vh - 50px)',
+        width: '100%',
+    },
+    buttonSignUp: {
+        borderRadius: 15,
+        backgroundColor: '#C54754',
+        color: 'white',
+    },
+    buttonLearnMore: {
+        borderRadius: 15,
+        backgroundColor: 'white',
+        border: '2px solid #FBA4AD',
+        color: '#C54754',
+    },
+    textHeading: {
+        color: '#C54754',
+        marginBottom: 20,
+    },
+    textSubheading: {
+        color: 'rgba(30, 74, 110, 0.9)',
+        marginBottom: 20,
+        fontSize: 30
+    },
+    noUnderline: {
+        textDecoration: 'none'
+    }
 }));
 
 const IndexPage = () => {
   const classes = useStyles();
   return (
     <Layout>
-      <SEO title="Home" />
-      <Paper className={classes.imgBackground}>
-        <h1>
-Opening your room to
-          <br />
-a world of possibilities
-        </h1>
-        <p>
-An app made for hotels
-          <br />
-So travelers can worry less and travel more.
-        </p>
-        <button>
-          <Link to="/sign-up/">Sign Up</Link>
-        </button>
-        <button>
-          <Link to="/learn-more/">Learn More</Link>
-        </button>
-      </Paper>
+        <SEO title="Home" />
+        <Paper className={classes.imgBackground}>
+            <Grid container xs={12} lg={7} direction="column" alignItems="center" className={classes.gridWrapper}>
+                <Grid item>
+                    <Typography variant="h2" className={classes.textHeading}>Opening your room to<br/>a world of possibilities</Typography>
+                    <Typography variant="subtitle1" className={classes.textSubheading}>
+                        An App made for <strong>Hotels</strong><br/>So travelers can <strong>worry less and travel more.</strong>
+                    </Typography>
+                </Grid>
+                <Grid container direction="row" justify="space-around">
+                    <Grid item xs={12} lg={4}>
+                        <Link to="/sign-up" className={classes.noUnderline}>
+                            <Button fullWidth variant="contained" className={classes.buttonSignUp}>
+                                Sign Up
+                            </Button>
+                        </Link>
+                    </Grid>
+                    <Grid item xs={12} lg={4}>
+                        <Link to="/monggo-io" className={classes.noUnderline}>
+                            <Button fullWidth variant="contained" className={classes.buttonLearnMore}>Learn More</Button>
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Paper>
     </Layout>
   );
 };
