428da8192f6a7b3680cfa313c4bf6bf1b0ff27a3
Kevin Kusuma <kevin.kusuma@revelsystems.com>
2019-09-17 20:06:52 -0400

    Added header, some parts of the home page, updated gatsby-config, added eslint for code styling.

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,14 +5,14 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import Header from "./header"
-//change css to scss
-//import "./layout.css"
-import "./layout.scss"
+import Header from './header';
+// change css to scss
+// import "./layout.css"
+import './layout.scss';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -23,32 +23,32 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
         }}
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Monggo.io
+          ©
+          {' '}
+          {new Date().getFullYear()}
+, Monggo.io
           {/* {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a> */}
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,46 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-import Layout from "../components/layout"
-// import Image from "../components/image"
-import SEO from "../components/seo"
+const homeBackground = '/images/background.jpg';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Opening your room to<br/>a world of possibilities</h1>
-    <p>An app made for hotels<br/>So travelers can worry less and travel more.</p>
-    <button>
-      <Link to="/sign-up/">Sign Up</Link>
-    </button>
-    <button>
-      <Link to="/learn-more/">Learn More</Link>
-    </button>
-    
-    {/* do not need this
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> */}
-  </Layout>
-)
+const useStyles = makeStyles((theme) => ({
+  imgBackground: {
+    background: `url(${homeBackground}) no-repeat center center fixed`,
+    backgroundSize: 'cover',
+    height: 'calc(100vh - 50px)',
+    width: '100%',
+  },
+}));
 
-export default IndexPage
+const IndexPage = () => {
+  const classes = useStyles();
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Paper className={classes.imgBackground}>
+        <h1>
+Opening your room to
+          <br />
+a world of possibilities
+        </h1>
+        <p>
+An app made for hotels
+          <br />
+So travelers can worry less and travel more.
+        </p>
+        <button>
+          <Link to="/sign-up/">Sign Up</Link>
+        </button>
+        <button>
+          <Link to="/learn-more/">Learn More</Link>
+        </button>
+      </Paper>
+    </Layout>
+  );
+};
+
+export default IndexPage;
