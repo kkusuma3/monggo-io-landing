79927a6aa36b88e7c8a09008813f244c00e43d80
Kevin Kusuma <kevin.kusuma@revelsystems.com>
2019-10-03 21:50:22 -0400

    Finished about page with cards and images and links to social media.

--- src/components/cardAbout.js
+++ src/components/cardAbout.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles({
     cardWrapper: {
-        margin: '10px 25px'
+        margin: '30px 50px'
+    },
+    innerWrapper: {
+        margin: '20px 40px'
     },
     card: {
         borderRadius: 15,
         border: '1px solid #C70000',
-        margin: '20px 25px',
         width: '100%'
     },
     title: {
@@ -36,35 +37,37 @@ const useStyles = makeStyles({
         marginTop: 10,
         marginBottom: 0,
         borderTop: '2px solid #C54754',
-    }
+    },
 });
 
-export default function CardWithText({title, description, image}) {
+export default function CardWithText({title, description, image, imageAlt}) {
     const classes = useStyles();
 
     return (
         <Grid container xs={12} className={classes.cardWrapper}>
             <Card className={classes.card}>
-                <CardContent>
-                    <Grid container xs={12} direction="column">
-                        <Grid item>
-                            <Typography variant="h4" className={classes.title}>
-                                {title}
-                            </Typography>
-                            <hr className={classes.titleUnderline}></hr>
+                <Grid container direction="row" xs={12} className={classes.innerWrapper}>
+                <Grid item xs={9}>
+                    <CardContent>
+                        <Grid container xs={12} direction="column">
+                            <Grid item>
+                                <Typography variant="h4" className={classes.title}>
+                                    {title}
+                                </Typography>
+                                <hr className={classes.titleUnderline}></hr>
+                            </Grid>
+                            <Grid item>
+                                <Typography variant="subtitle1" className={classes.description}>
+                                    {description}
+                                </Typography>
+                            </Grid>
                         </Grid>
-                        <Grid item>
-                            <Typography variant="subtitle1" className={classes.description}>
-                                {description}
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </CardContent>
-                <CardMedia
-                    className={classes.cover}
-                    image={image}
-                    title="Pillars Logo"
-                />
+                    </CardContent>
+                </Grid>
+                <Grid item xs={2}>
+                    <img src={image} alt={imageAlt} />
+                </Grid>
+                </Grid>
             </Card>
         </Grid>
     );
@@ -74,4 +77,5 @@ CardWithText.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
+    imageAlt: PropTypes.string.isRequired,
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,10 +5,12 @@ import { Typography, Grid } from '@material-ui/core';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import CardAbout from '../components/cardAbout';
+import CardTeam from '../components/cardTeam';
 import monggoLogo from '../images/logo_transparent.png';
 import ConnectingImage from '../images/problem_solving.png';
 import ComfortImage from '../images/adventure_1.png';
 import ConsistentImage from '../images/city_building_1.png';
+import KevinImage from '../images/img-kevin.jpg';
 
 const useStyles = makeStyles((theme) => ({
     logo: {
@@ -17,15 +19,19 @@ const useStyles = makeStyles((theme) => ({
     },
     textTitle: {
         color: '#C54754',
-        margin: 30,
+        margin: '10px 50px',
         marginTop: 0,
+        fontSize: 20
     },
     titleUnderline: {
         margin: 'auto',
-        width: 150,
+        width: 200,
         marginTop: 40,
         marginBottom: 0,
         borderTop: '2px solid #C54754',
+    },
+    cardTeamWrapper: {
+        margin: '20px 0'
     }
 }));
 
@@ -49,18 +55,21 @@ const MonggoIo = () => {
                 <Grid container xs={12} justify="center">
                     <CardAbout 
                         title="Connecting the dots" 
-                        description="Lorem ipsuasjdnasjndjajdan asdasdad sadsad a"
+                        description="We aim to build a bridge between hotel and customers where deemed necessary to create a pleasant experience for both parties. This includes language translation, online ordering platform and even more coming!"
                         image={ConnectingImage}
+                        imageAlt="Connecting puzzles"
                     />
                     <CardAbout 
                         title="Comfort Zone for Customers" 
-                        description="Lorem ipsuasjdnasjndjajdan asdasdad sadsad a"
+                        description="As avid travelers ourselves, we know traveling can be problematic when coupled with a language barrier. Therefore, we envision an ecosystem where travelers can travel without fear of miscommunication especially when it comes to accommodations."
                         image={ComfortImage}
+                        imageAlt="Adventure seeking"
                     />
                     <CardAbout 
                         title="Consistent Hotel Experience" 
-                        description="Lorem ipsuasjdnasjndjajdan asdasdad sadsad a"
+                        description="Our goal is to build a universal platform for hotels around the globe to keep it consistent for you, wherever you go. Going to Europe? Asia? Don’t worry,  you’ll still be able to understand everything. We've got you covered."
                         image={ConsistentImage}
+                        imageAlt="City skycrapers"
                     />
                 </Grid>
             </Grid>
@@ -69,6 +78,43 @@ const MonggoIo = () => {
                     <hr className={classes.titleUnderline}></hr>
                     <Typography variant="subtitle1" className={classes.textTitle}>Meet the Team</Typography>
                 </Grid>
+            </Grid>
+            <Grid container direction="row" xs={12} justify="space-around" spacing={4} className={classes.cardTeamWrapper}>
+                <CardTeam 
+                    name="Gideon Yuwono"
+                    jobTitle="Co-Founder/ Tech Lead"
+                    image={KevinImage}
+                    imageAlt="Portrait of Gideon Yuwono"
+                />
+                <CardTeam 
+                    name="Kevin Kusuma"
+                    jobTitle="Co-Founder/ Tech Lead"
+                    image={KevinImage}
+                    imageAlt="Portrait of Kevin Kusuma"
+                    fbLink="https://www.facebook.com/kevindk20"
+                    linkedInLink="https://www.linkedin.com/in/kevinkusuma"
+                    emailLink="mailto:kevindwiputrak@gmail.com"
+                />
+                <CardTeam 
+                    name="Melvin Yuwono"
+                    jobTitle="Co-Founder/ Tech Lead"
+                    image={KevinImage}
+                    imageAlt="Portrait of Kevin Kusuma"
+                />
+            </Grid>
+            <Grid container direction="row" xs={12} justify="space-around" spacing={4} className={classes.cardTeamWrapper}>
+                <CardTeam 
+                    name="Yosias Paskal"
+                    jobTitle="Software Engineer"
+                    image={KevinImage}
+                    imageAlt="Portrait of Yosias Paskal"
+                />
+                <CardTeam 
+                    name="Stefamikha Suwisar"
+                    jobTitle="Lead Designer"
+                    image={KevinImage}
+                    imageAlt="Portrait of Stefamikha Suwisar"
+                />
             </Grid>
         </Layout>
     )
